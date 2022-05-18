a3e40fcdb9a4e44f163ddb7e0b185836d25a6cdf
Saravana Kumar Periyasamy <psaravana@vmware.com>
2022-05-18 15:01:09 -0500

    created a api from kubebuilder

--- main.go
+++ main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	iblogdevv1 "github.com/iblogdev/kubebuilder-example/apis/iblog.dev/v1"
+	iblogdevcontrollers "github.com/iblogdev/kubebuilder-example/controllers/iblog.dev"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(iblogdevv1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&iblogdevcontrollers.ExampleReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Example")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
